9da1fa92b13f5330251388c2dbf99b822b806357
UEMURA <mr.dagonn@gmail.com>
2014-06-09 16:21:32 +0900

    DataPackを修正。
    範囲forとか追加

--- Utility/DataPack.h
+++ Utility/DataPack.h
@@ -1,4 +1,5 @@
 #pragma once//?Unicode
+#include <array>
 
 namespace SDX
 {
@@ -7,12 +8,64 @@ namespace SDX
     class DataPack
     {
     private:
-        TData dataS[(int)TType::MAX];
+        std::array<TData, (int)TType::MAX> Array;
     public:
+        size_t size() const
+        {
+            return Array.size();
+        }
 
         TData& operator[](const TType index)
         {
-            return dataS[(int)index];
+            return Array[(int)index];
         }
+
+        const TData& operator[](const TType index) const
+        {
+            return Array[(int)index];
+        }
+
+#ifdef __WINDOWS__
+        std::_Array_iterator<TData, (int)TType::MAX> begin()
+        {
+            return Array.begin();
+        }
+
+        std::_Array_iterator<TData, (int)TType::MAX> end()
+        {
+            return Array.end();
+        }
+
+        std::_Array_const_iterator<TData, (int)TType::MAX> begin() const
+        {
+            return Array.begin();
+        }
+
+        std::_Array_const_iterator<TData, (int)TType::MAX> end() const
+        {
+            return Array.end();
+        }
+#else
+        auto begin()
+        {
+            return Array.begin();
+        }
+
+        auto end()
+        {
+            return Array.end();
+        }
+
+        auto begin() const
+        {
+            return Array.begin();
+        }
+
+        auto end() const
+        {
+            return Array.end();
+        }
+#endif
+
     };
 }
